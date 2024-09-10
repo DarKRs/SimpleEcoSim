4f5a49dc30f20f56802896639862e960bcf84410
DarKRs <centralstreet_24@mail.ru>
2024-09-10 14:45:11 +0600

    Улучшил отрисовку

--- SimpleEcoSim/Program.cs
+++ SimpleEcoSim/Program.cs
@@ -1,17 +1,18 @@
 ﻿using SimpleEcoSim.Modles;
+using SimpleEcoSim.Services;
 using System;
 
 namespace SimpleEcoSim
 {
     internal class Program
     {
-        static char[,] buffer;
+
 
         static void Main(string[] args)
         {
+            Console.Clear();
+            ConsoleService.Init();
             List<Entity> items = new List<Entity>();
-            SetConsoleSize();
-            InitializeBuffer();
             Random rnd = new Random();
             for(int i = 0; i < rnd.Next(20); i++)
             {
@@ -21,81 +22,18 @@ namespace SimpleEcoSim
             {
                 while (!Console.KeyAvailable)
                 {
-                    DrawAll(items);
+                    ConsoleService.DrawAll(items);
+                    Thread.Sleep(1000);
+                    items.Add(new Plant(Tuple.Create(rnd.Next(120), rnd.Next(40))));
                     Thread.Sleep(1000);
                 }
             }
             while (Console.ReadKey(true).Key != ConsoleKey.Q);
         }
 
-        static void DrawAll(List<Entity> items) {
-            InitializeBuffer();
-            foreach (Entity item in items)
-            {
-                buffer[item.pos.Item1, item.pos.Item2] = item.Sign;
-            }
-            RenderBuffer();
-        }
-
-        static void RenderBuffer()
-        {
-            for (int y = 0; y < Console.WindowHeight; y++)
-            {
-                for (int x = 0; x < Console.WindowWidth; x++)
-                {
-                    Console.SetCursorPosition(x, y);
-                    Console.Write(buffer[x, y]);
-                }
-            }
-        }
-
-
-        static void InitializeBuffer()
-        {
-            buffer = new char[Console.WindowWidth, Console.WindowHeight];
-            for (int i = 0; i < Console.WindowWidth; i++)
-            {
-                for (int j = 0; j < Console.WindowHeight; j++)
-                {
-                    buffer[i, j] = '.';
-                }
-            }
-        }
 
 
 
-        static void SetConsoleSize()
-        {
-            var currentBufferSize = Console.BufferWidth;
-            var currentWindowSize = Console.WindowWidth;
 
-            const int maxBufferWidth = 8191;
-            const int maxBufferHeight = 30000;
-            const int maxWindowWidth = 8191;
-            const int maxWindowHeight = 30000;
-
-            // Установка размеров буфера и окна
-            int newBufferWidth = 120; 
-            int newBufferHeight = 40;
-            int newWindowWidth = 120;
-            int newWindowHeight = 40;
-
-            try
-            {
-                Console.SetBufferSize(
-                    Math.Min(newBufferWidth, maxBufferWidth),
-                    Math.Min(newBufferHeight, maxBufferHeight)
-                );
-
-                Console.SetWindowSize(
-                    Math.Min(newWindowWidth, maxWindowWidth),
-                    Math.Min(newWindowHeight, maxWindowHeight)
-                );
-            }
-            catch (ArgumentOutOfRangeException ex)
-            {
-                Console.WriteLine($"Error setting console size: {ex.Message}");
-            }
-        }
     }
 }
---
+++ SimpleEcoSim/Services/ConsoleService.cs
@@ -0,0 +1,107 @@
+﻿using SimpleEcoSim.Modles;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleEcoSim.Services
+{
+    internal static class ConsoleService
+    {
+        static List<Rectangle> dirtyRectangles = new List<Rectangle>();
+        static char[,] buffer;
+
+        public static void Init()
+        {
+            SetConsoleSize();
+            InitializeBuffer();
+            RenderBuffer();
+        }
+
+        public static void DrawAll(List<Entity> items)
+        {
+            dirtyRectangles.Clear();
+            foreach (Entity item in items)
+            {
+                buffer[item.pos.Item1, item.pos.Item2] = item.Sign;
+                dirtyRectangles.Add(new Rectangle(item.pos.Item1, item.pos.Item2, 1, 1));
+            }
+            RenderNew();
+        }
+
+        static void RenderNew()
+        {
+            foreach (var rect in dirtyRectangles)
+            {
+                Console.SetCursorPosition(rect.X, rect.Y);
+                Console.Write(buffer[rect.X, rect.Y]);
+            }
+        }
+
+
+        static void RenderBuffer()
+        {
+            for (int y = 0; y < Console.WindowHeight; y++)
+            {
+                for (int x = 0; x < Console.WindowWidth; x++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.Write(buffer[x, y]);
+                }
+
+            }
+        }
+
+
+
+
+        static void InitializeBuffer()
+        {
+            buffer = new char[Console.WindowWidth, Console.WindowHeight];
+            for (int i = 0; i < Console.WindowWidth; i++)
+            {
+                for (int j = 0; j < Console.WindowHeight; j++)
+                {
+                    buffer[i, j] = '.';
+                }
+            }
+        }
+
+        static void SetConsoleSize()
+        {
+            var currentBufferSize = Console.BufferWidth;
+            var currentWindowSize = Console.WindowWidth;
+
+            const int maxBufferWidth = 8191;
+            const int maxBufferHeight = 30000;
+            const int maxWindowWidth = 8191;
+            const int maxWindowHeight = 30000;
+
+            // Установка размеров буфера и окна
+            int newBufferWidth = 120;
+            int newBufferHeight = 40;
+            int newWindowWidth = 120;
+            int newWindowHeight = 40;
+
+            try
+            {
+                Console.SetBufferSize(
+                    Math.Min(newBufferWidth, maxBufferWidth),
+                    Math.Min(newBufferHeight, maxBufferHeight)
+                );
+
+                Console.SetWindowSize(
+                    Math.Min(newWindowWidth, maxWindowWidth),
+                    Math.Min(newWindowHeight, maxWindowHeight)
+                );
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine($"Error setting console size: {ex.Message}");
+            }
+        }
+
+    }
+}
